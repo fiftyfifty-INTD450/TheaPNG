ba1e984ff56e73c7e4efc21a77c5a6e967231ee4
SethBergen <sbergen@ualberta.ca>
2019-03-12 02:38:38 -0600

    Fixed file system not working in build
    Moved FileSys folder into StreamingAssets and re-wrote asset loading scripts

--- UNTITLED/Assets/Scripts/Classes/FSEImage.cs
+++ UNTITLED/Assets/Scripts/Classes/FSEImage.cs
@@ -5,9 +5,23 @@ using UnityEngine.UI;
 
 public class FSEImage : FileSysElement
 {
+    public GameObject imagePanel;
+
     public void OpenImage()
     {
-        // TODO
         Debug.Log("Opening Image: "+(this.GetComponentInChildren(typeof(Text)) as Text).text);
+
+        /*string imagePath = path.Replace("Assets/Resources/", "");
+        int idx = imagePath.LastIndexOf(".");
+        if (idx > 0)
+        {
+            imagePath = imagePath.Substring(0, idx);
+        }*/
+
+
+        Debug.Log("Setting Image from: " + path);
+
+        imagePanel.GetComponent<ImagePanel>().SetImage(path);
+        imagePanel.SetActive(true);
     }
 }
---
+++ UNTITLED/Assets/Scripts/Classes/ImagePanel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+[RequireComponent(typeof(Image))]
+public class ImagePanel : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private Texture2D LoadTexture(string path)
+    {
+        Texture2D tex;
+        byte[] FileData;
+
+        if (File.Exists(path))
+        {
+            FileData = File.ReadAllBytes(path);
+            tex = new Texture2D(1, 1);
+            if (tex.LoadImage(FileData))
+                return tex;
+        }
+        return null;
+    }
+
+    public Sprite LoadSprite(string path, float pixelsPerUnit = 100.0f)
+    {
+
+        // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
+
+        Sprite sprite;
+        Texture2D texture = LoadTexture(path);
+        sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0), pixelsPerUnit);
+
+        return sprite;
+    }
+
+    public void SetImage(string path)
+    {
+        Sprite panelImage = LoadSprite(path);
+
+        if (!panelImage) Debug.Log("FILE ERROR No file at path \"" + path + "\"");
+
+        transform.GetChild(0).GetComponent<Image>().sprite = panelImage;
+        transform.GetChild(0).GetComponent<Image>().preserveAspect = true;
+    }
+}
