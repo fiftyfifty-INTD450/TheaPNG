b0383319525975a54f29725cb4e5f433b80e96ef
SethBergen <sbergen@ualberta.ca>
2019-04-11 20:43:09 -0600

    Added new click SFX

---
+++ UNTITLED/Assets/Scripts/Managers/SFXManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+
+    public AudioClip audioMouseClick;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = audioMouseClick;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            audioSource.Play();
+        }
+    }
+}
