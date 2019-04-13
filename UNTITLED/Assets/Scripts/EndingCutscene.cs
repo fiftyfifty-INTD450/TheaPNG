baef8adb9a38eae2d48ec779fd9f1cce9c5723b8
Ken Li <krli@ualberta.ca>
2019-04-12 18:17:06 -0600

    Linked All Ending Cutscenes

--- UNTITLED/Assets/Scripts/EndingCutscene.cs
+++ UNTITLED/Assets/Scripts/EndingCutscene.cs
@@ -1,19 +1,90 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 using UnityEngine.SceneManagement;
 
 public class EndingCutscene : MonoBehaviour
 {
+	public GameObject everything;
+	public GameObject sfxManager;
+
+	public GameObject warning;
 	public GameObject prompt;
 	public GameObject progress;
 
 	public AudioSource audioPlayer;
 	public AudioClip endingVO;
 
+	public VideoPlayer videoPlayer;
+	public VideoClip deleteClip;
+	public VideoClip yesClip;
+
 	public Slider progressSlider;
 
 	public GameObject fadeBlack;
+
+	public void Start()
+	{
+		ApplicationModel.SetFileSysHead("Diary");
+
+		StartCoroutine(DeleteClip());
+	}
+
+	IEnumerator DeleteClip()
+	{
+		everything.SetActive(false);
+		sfxManager.SetActive(false);
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+
+		double videoDuration = deleteClip.length;
+
+		videoPlayer.clip = deleteClip;
+		videoPlayer.Play();
+
+		yield return new WaitForSeconds((float) videoDuration);
+
+		videoPlayer.Stop();
+
+		everything.SetActive(true);
+		sfxManager.SetActive(true);
+
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
+	IEnumerator YesClip()
+	{
+		everything.SetActive(false);
+		sfxManager.SetActive(false);
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+
+		double videoDuration = yesClip.length;
+
+		videoPlayer.clip = yesClip;
+		videoPlayer.Play();
+
+		yield return new WaitForSeconds((float) videoDuration);
+
+		videoPlayer.Stop();
+
+		everything.SetActive(true);
+		sfxManager.SetActive(true);
+
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+
+		ShowProgress();
+	}
+
+	public void ShowPrompt()
+	{
+		warning.SetActive(true);
+	}
 
 	public void ShowProgress()
 	{
@@ -21,6 +92,11 @@ public class EndingCutscene : MonoBehaviour
 		progress.SetActive(true);
 
 		StartCoroutine(ProgressSlider());
+	}
+
+	public void ForceDelete()
+	{
+		StartCoroutine(YesClip());
 	}
 
 	IEnumerator ProgressSlider()
